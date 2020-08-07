b0d60cfd421ca22378777161ddb969c452258aa2
pderakhshanfar <pouria.derakhshanfar@gmail.com>
2020-08-07 14:59:11 +0200

    Add docker-related files

---
+++ docker-run.sh
@@ -0,0 +1,23 @@
+experiment=$1
+LIMIT=$2
+# IFS=$'\n'
+# Stop the container from previous experiments
+docker stop exrunner-container
+# Remove old containers
+docker rm exrunner-container
+# Remove previous docker image
+docker rmi tudelft/exrunner
+# Build a new docker image
+docker image build -t tudelft/exrunner $(pwd)
+# Execution
+# After building the the image, we run the container
+docker run -dit --name exrunner-container  \
+--mount type=bind,source="$(pwd)/$experiment/consoleLog",target=/reproduction/$experiment/consoleLog \
+--mount type=bind,source="$(pwd)/$experiment/logs",target=/reproduction/$experiment/logs \
+--mount type=bind,source="$(pwd)/$experiment/results",target=/reproduction/$experiment/results \
+--mount type=bind,source="$(pwd)/crashes",target=/reproduction/crashes \
+--mount type=bind,source="$(pwd)/bins",target=/reproduction/bins \
+tudelft/exrunner
+# Execute main.sh in the running container
+ docker exec -it exrunner-container bash -c "cd $experiment; bash main.sh $LIMIT > consoleLog/consoleOut.txt 2> consoleLog/consoleErr.txt"
+echo "Done!"
